1144d07be4c56bd5426918be26ea6ca2ec1d6940
Srikanthnaid <ksru044gmail.com>
2022-09-24 16:19:08 +0530

    Added: interface of hotel reservstion method.

---
+++ com/HotelReservation/HotelReservationIF.java
@@ -0,0 +1,13 @@
+package com.HotelReservation;
+
+import java.util.ArrayList;
+
+public interface HotelReservationIF {
+	public void addHotel(String hotelName, int rating, double regularCustomerRate);
+
+	public int getHotelListSize();
+
+	public void printHotelList();
+
+	public ArrayList<Hotel> getHotelList();
+}
